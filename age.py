b142fb4f173232085519b723423209f752317437
Jack Hung <880816jack@gmail.com>
2022-07-23 13:03:23 +0800

    更新else if判斷程式跟bmi計算程式

---
+++ age.py
@@ -0,0 +1,34 @@
+#%%   #else if:  #另外如果 python 打elif
+age = input('請輸入年齡:')
+age = int(age) #型別轉換:把str轉換成int
+if age < 13 :
+    print('國小')
+elif age >= 13 and age <18: #另外如果
+    print('國高中')
+elif age >= 18 and age <22: #另外如果
+    print('大學')
+else:
+    print('出社會')
+
+# %%
+#計算bmi
+w = input('請輸入體重:')
+h = input('請輸入身高')
+w = float(w)
+h = float(h)
+height = h / 100
+bmi = w / (height * height)
+if bmi < 18.5:
+    print('你的bmi值為', bmi ,'屬於體重過輕')
+elif bmi >= 18.5 and bmi < 24: 
+    print('你的bmi值為', bmi ,'屬於正常範圍')
+elif bmi >= 24 and bmi < 27: 
+    print('你的bmi值為', bmi ,'稍重')
+elif bmi >= 27 and bmi < 30: 
+    print('你的bmi值為', bmi ,'輕度肥胖')
+elif bmi >= 30 and bmi < 35: 
+    print('你的bmi值為', bmi ,'中度肥胖')
+elif bmi >= 35 and bmi < 40: 
+    print('你的bmi值為', bmi ,'重度肥胖')
+
+# %%
