84405fc452e7298bf46c92289841c640f7b9e861
Jack Hung <880816jack@gmail.com>
2022-07-23 20:19:48 +0800

    新增寫入檔案到電腦

--- product.py
+++ product.py
@@ -6,12 +6,18 @@ while True:
        break
     #建立二維度清單
     price = input('請輸入價格:')
+    price = int(price)
     p = [name,price] #p是小清單
     products.append(p) #把小清單裝到大清單裡面
 print(products[0][0])  #取大清單的第一項的第一小項
 
 for p in products:
     print(p[0],'的價格是',p[1]) # 取小清單的第一項跟第二項
-    
+#寫到電腦檔案
+with open('product.csv','w') as f:  #open:打開檔案的意思 csv可以存成excel檔  因為一個檔案有開就一定要關掉，所以with的功能是幫檔案自動close，跳出with的範圍就不能再寫入了
+    for p in products:
+        f.write(p[0] + ',' +  str(p[1]) + '\n')  #寫入
+        
+
 
 # %%
