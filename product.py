8419f91a2ebb4f2cdda3c6da2209f9a5d955192b
Jack Hung <880816jack@gmail.com>
2022-07-26 16:09:31 +0800

    讀取檔案部讀取標題欄

--- product.py
+++ product.py
@@ -4,12 +4,14 @@
 products = []
 with open('product.csv','r',encoding= 'UTF-8') as f:
     for line in f:
+        if '商品,價格' in line:
+            continue  #繼續，跳到下一次
         name , price = line.strip().split(',')  #先用split把多餘的空格去掉，再用split切割(因為切割完變成兩塊，所以用兩個變數去存)
         products.append([name,price])
 print(products)
 
 
-
+# 讓使用者輸入
 while True:
     name = input('請輸入商品名稱:')
     if name == 'q': #quit
@@ -21,11 +23,16 @@ while True:
     products.append(p) #把小清單裝到大清單裡面
 print(products[0][0])  #取大清單的第一項的第一小項
 
+
+#印出所有購買紀錄
 for p in products:
     print(p[0],'的價格是',p[1]) # 取小清單的第一項跟第二項
 #寫到電腦檔案
 with open('product.csv','w',encoding = 'UTF-8') as f:  #open:打開檔案的意思 csv可以存成excel檔  因為一個檔案有開就一定要關掉，所以with的功能是幫檔案自動close，跳出with的範圍就不能再寫入了
                                                       # 要先去說明要用甚麼編碼寫入，之後excel也要調編碼(要用甚麼編碼讀取)
+   
+   
+#寫入檔案 
     f.write('商品,價格\n')                             #在excel檔最上方新增標題欄
     for p in products:
         f.write(p[0] + ',' +  str(p[1]) + '\n')  #寫入
