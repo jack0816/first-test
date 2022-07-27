be3a1b5e518f919426cf982786da0640474d88d7
Jack Hung <880816jack@gmail.com>
2022-07-27 10:09:39 +0800

    新增return用法、閏年練習(剛剛number的版本上傳錯檔案)

--- func.py
+++ func.py
@@ -35,4 +35,35 @@ def add(x=0, y=0):  #function的參數帶預設值，呼叫參數就不一定要
 
 add()
 
+# %% #return用法
+
+def add(x, y):
+    return x + y
+
+result = add(3,4)
+print(result)
+
+#算清單平均
+def avg(numbers):
+    sum(numbers)    #sum可以對清單用，就能算出清單總數
+    a =  sum(numbers) / len(numbers)
+    return a
+
+r = avg([2,3,4])
+print(r)
+
+# %%
+x = input('請輸入年分')
+x = int(x)
+
+def is_leep(x):
+    if x % 4 != 0:
+        return False
+    elif x % 4 == 0 and x % 100 != 0 :
+        return True
+    elif x % 100 == 0 and x % 400 != 0 :
+        return False
+    elif x % 400 == 0 and x % 3200 != 0:
+        return True
+print(is_leep(x))
 # %%
