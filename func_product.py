8b4ac92b4e811cf6385ee753385bf0b6e14c4ec4
Jack Hung <880816jack@gmail.com>
2022-07-27 17:59:21 +0800

    把product的程式碼寫成function

---
+++ func_product.py
@@ -0,0 +1,54 @@
+#%%
+import os  #operating system
+
+
+products = []
+def read_file(filename): #設參數filename是因為不要限制檔名就是哪一個，用傳遞的方式傳進來，不用寫死
+    if os.path.isfile(filename):  #確認檔案是否存在電腦裡
+        print('Yah,找到檔案了')
+
+        #讀取檔案
+        with open(filename,'r',encoding= 'UTF-8') as f:
+             for line in f:
+                if '商品,價格' in line:
+                    continue  #繼續，跳到下一次
+                name , price = line.strip().split(',')  #先用split把多餘的空格去掉，再用split切割(因為切割完變成兩塊，所以用兩個變數去存)
+                products.append([name,price])
+        print(products)
+
+    else:
+        print('找不到檔案')
+    return products #因為我們把讀到的東西都裝到products，所以要return products
+
+# 讓使用者輸入
+def user_input(products):
+    while True:
+        name = input('請輸入商品名稱:')
+        if name == 'q': #quit
+            break
+        #建立二維度清單
+        price = input('請輸入價格:')
+        price = int(price)
+        p = [name,price] #p是小清單
+        products.append(p) #把小清單裝到大清單裡面
+    print(products[0][0])  #取大清單的第一項的第一小項
+    return products  #因為有append東西進去，清單有改變，所以要return
+
+#印出所有購買紀錄
+def print_products(products):
+    for p in products:
+        print(p[0],'的價格是',p[1]) # 取小清單的第一項跟第二項
+#寫到電腦檔案
+def write_file(filename, products):
+    with open(filename,'w',encoding = 'UTF-8') as f:  #open:打開檔案的意思 csv可以存成excel檔  因為一個檔案有開就一定要關掉，所以with的功能是幫檔案自動close，跳出with的範圍就不能再寫入了
+                                                      # 要先去說明要用甚麼編碼寫入，之後excel也要調編碼(要用甚麼編碼讀取)
+
+    #寫入檔案 
+        f.write('商品,價格\n')                             #在excel檔最上方新增標題欄
+        for p in products:
+            f.write(p[0] + ',' +  str(p[1]) + '\n')  #寫入
+        
+products = read_file('product.csv')
+products = user_input(products)   #新的結果又存回products，取代掉之前的內容(尚義行的products)
+print_products(products)
+write_file('product.csv',products)
